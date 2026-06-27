95826aaec09397c9e8a5eddab4b02a6313ae7d7b
Shubham7085 <shubhamnagvanshi5@gmail.com>
2026-06-27 13:02:35 +0530

    Refactor login button click handler to use async/await

--- login.js
+++ login.js
@@ -1,19 +1,18 @@
 import { auth } from "./firebase.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-document.getElementById("loginBtn").onclick = function () {
+document.getElementById("loginBtn").addEventListener("click", async () => {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      document.getElementById("status").innerHTML = "✅ Login Successful";
-      window.location.href = "friends.html";
-    })
-    .catch((error) => {
-      document.getElementById("status").innerHTML =
-        "❌ " + error.message;
-    });
+        document.getElementById("status").innerHTML = "✅ Login Successful";
 
-};
+        window.location.href = "admin.html";
+    } catch (error) {
+        document.getElementById("status").innerHTML =
+            "❌ " + error.message;
+    }
+});
